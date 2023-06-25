bb89e990e5730688b4a3b292c046cd542b7a4294
GuillermoCastelao <guillermocastelao@gmail.com>
2023-06-25 18:11:24 -0300

    commit6

--- facundo/facundo/settings.py
+++ facundo/facundo/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-)sg9to8q)gwxv$r83rdqi@+4@s=bfr_$rdlu+3v$9qy)x)cbq$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.210.185.164', 'localhost', '127.0.0.1']
 
 
 # Application definition
